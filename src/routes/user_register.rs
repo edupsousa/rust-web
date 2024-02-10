f1c82cdba673d332350f54cb1a65e2522df23b6b
Eduardo P de Sousa <edupsousa@gmail.com>
2024-02-10 20:06:31 -0300

    Add db.sqlite3 to .gitignore and update user_register.rs

--- src/routes/user_register.rs
+++ src/routes/user_register.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
 
-use axum::{response::Response, Extension, Form};
-use serde::{Deserialize, Serialize};
 use crate::templates::{render_response, TemplateEngine};
+use axum::{
+    response::{IntoResponse, Redirect, Response},
+    Extension, Form,
+};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Default, Debug, Clone)]
 pub struct RegisterForm {
@@ -16,13 +20,13 @@ type FormErrors = HashMap<&'static str, &'static str>;
 impl RegisterForm {
     fn get_errors(&self) -> FormErrors {
         let mut errors = FormErrors::default();
-        
+
         if self.email.is_empty() {
             errors.insert("email", "Email is required");
         } else if !validator::validate_email(&self.email) {
             errors.insert("email", "Invalid email address");
         }
-        
+
         if self.password.len() < 8 {
             errors.insert("password", "Password must be at least 8 characters long");
         }
@@ -44,17 +48,36 @@ pub struct RegisterPageData {
 }
 
 pub async fn get(Extension(template_engine): Extension<TemplateEngine>) -> Response {
-    render_response(&template_engine, "user/register", &RegisterPageData::default())
+    render_response(
+        &template_engine,
+        "user/register",
+        &RegisterPageData::default(),
+    )
 }
 
 pub async fn post(
     Extension(template_engine): Extension<TemplateEngine>,
+    Extension(db): Extension<DatabaseConnection>,
     Form(form): Form<RegisterForm>,
 ) -> Response {
     let errors: FormErrors = form.get_errors();
-    let data = RegisterPageData {
-        form,
-        errors,
-    };
+    if errors.is_empty() {
+        create_user(&db, &form).await;
+        return Redirect::to("/user/login?registered=true").into_response();
+    }
+    let data = RegisterPageData { form, errors };
     render_response(&template_engine, "user/register", &data)
+}
+
+use entity::user;
+
+pub async fn create_user(db: &DatabaseConnection, form: &RegisterForm) {
+    user::ActiveModel {
+        email: Set(form.email.to_owned()),
+        password: Set(form.password.to_owned()),
+        ..Default::default()
+    }
+    .save(db)
+    .await
+    .expect("Failed to save user");
 }
