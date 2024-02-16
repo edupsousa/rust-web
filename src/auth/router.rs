f183ac00256bf323d9371851bfdbf8326d6380be
Eduardo P de Sousa <edupsousa@gmail.com>
2024-02-16 14:55:32 -0300

    Add middleware to invalidate certain routes when user is signed in

--- src/auth/router.rs
+++ src/auth/router.rs
@@ -1,10 +1,14 @@
 use crate::app::AppState;
 
 use super::{
+    layer::AuthSession,
     login_page::{get_login, get_logout, post_login},
     register_page::{get_register, post_register},
 };
 use axum::{
+    extract::Request,
+    middleware::{self, Next},
+    response::{IntoResponse, Redirect, Response},
     routing::{get, post},
     Router,
 };
@@ -13,7 +17,20 @@ pub fn router() -> Router<AppState> {
     Router::new()
         .route("/login", get(get_login))
         .route("/login", post(post_login))
-        .route("/logout", get(get_logout))
         .route("/register", get(get_register))
         .route("/register", post(post_register))
+        .layer(middleware::from_fn(invalid_when_signed_in))
+        .route("/logout", get(get_logout))
+}
+
+async fn invalid_when_signed_in(
+    auth_session: AuthSession,
+    request: Request,
+    next: Next,
+) -> Response {
+    if auth_session.user.is_some() {
+        return Redirect::to("/").into_response();
+    }
+
+    next.run(request).await
 }
